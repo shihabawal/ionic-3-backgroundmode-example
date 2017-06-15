90a709be29f96cfb9bd9e6d872f9803a6bf0c407
Shihab Awal <Shihab Awal>
2017-06-15 04:48:24 +0400

    First

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,23 +7,33 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  
+  delayNum;
+  time;
+  worker;
 
   constructor(public navCtrl: NavController, private backGroundMode:BackgroundMode) {
     
   }
   
-  time = new Date().getTime();
-  worker;
-  
   ngAfterViewInit() {
+    
+  }
+  
+  startBGMode() {
     if (!this.backGroundMode.isEnabled()){
+      this.time = new Date().getTime();
       this.backGroundMode.enable()
       this.backGroundMode.on('activate').subscribe(() => {
-        this.worker = setInterval(function (time,bg: BackgroundMode) {
-          if ((new Date().getTime() - time) > 10000){
+        this.worker = setInterval(function (time,bg: BackgroundMode, delay) {
+          console.log(new Date().getTime() - time);
+          if ((new Date().getTime() - time) > delay){
+            bg.wakeUp();
+            bg.unlock();
             bg.moveToForeground();
+            bg.disable();
           };
-        },1000, this.time, this.backGroundMode);
+        },1000, this.time, this.backGroundMode, this.delayNum);
       });
       
       this.backGroundMode.on('deactivate').subscribe(() => {
@@ -40,5 +50,4 @@ export class HomePage {
     document.getElementById('isActive').innerHTML = this.backGroundMode.isActive() ? "Active" : "Inactive" ;
     document.getElementById('isEnabled').innerHTML = this.backGroundMode.isEnabled() ? "Enabled" : "Disabled" ;
   }
-
 }
